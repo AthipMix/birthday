896a2dbecf71a2d979f5168b54a948cff93ab96a
AthipMix <674259028@webmail.npru.ac.th>
2025-09-05 22:25:53 +0700

    Happy Birthday My Love

---
+++ P1/RandomNumber.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class RandomNumber {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int randomNumber = random.nextInt(100) + 1; 
+        Scanner scanner = new Scanner(System.in);
+        int guess = 0;
+
+        System.out.println("Guess the number between 1 and 100:");
+
+        while (guess != randomNumber) {
+            System.out.print("Enter your guess: ");
+            guess = scanner.nextInt();
+
+            if (guess < randomNumber) {
+                System.out.println("Too low! Try again.");
+            } else if (guess > randomNumber) {
+                System.out.println("Too high! Try again.");
+            } else {
+                System.out.println("Congratulations! You guessed the correct number.");
+            }
+        }
+
+        scanner.close();
+    }
+}
